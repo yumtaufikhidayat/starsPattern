2402bfd3cd8541fd22f833c1f8c238071b25c2a7
yumtaufikhidayat <yumtaufik1997@gmail.com>
2019-05-26 04:25:18 +0700

    Draw the asterisk pattern by using odd number in Python

---
+++ starsPatternPython.py
@@ -0,0 +1,15 @@
+def xprint(size):
+    i,j = 0, size - 1
+
+    while j>= 0 and i<size:
+        inisial_spasi = ' '*min(i,j)
+        tengah_spasi = ' '*(abs(i-j)-1)
+        final_spasi = ' '*(size-1-max(i,j))
+
+        if j==i:
+            print inisial_spasi + '*' + final_spasi
+        else:
+            print inisial_spasi + '*' + tengah_spasi + '*' + final_spasi    
+        i += 1
+        j -= 1
+xprint(4)
